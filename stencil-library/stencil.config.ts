b980965cdb0683106f03c258e8ba9f63022832ba
Jack Sloan <hi@jacksloan.dev>
2022-06-16 17:51:04 -0500

    add minimal stencil configuration to generate an angular library

--- stencil-library/stencil.config.ts
+++ stencil-library/stencil.config.ts
@@ -1,10 +1,16 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'stencil-library',
   globalScript: 'src/global/app.ts',
   globalStyle: 'src/global/app.css',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      directivesProxyFile: '../angular-target/src/app/stencil-library/generated/proxies.ts',
+      directivesArrayFile: '../angular-target/src/app/stencil-library/generated/index.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
